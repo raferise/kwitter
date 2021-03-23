48a6ba1de895110e45ab0830bce39d2e3a7cab9e
Riley Ferise <raferise@gmail.com>
2021-03-23 12:48:27 -0400

    modified login and loadMoreMessages

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,33 @@
 import create from "zustand";
 import { devtools } from "zustand/middleware";
+import { getMessages, getUser, loginRequest } from "../fetchRequests";
 
 // define reducer function
 const reducer = (set) => ({
   user: {token: ""},
   messages: [],
-  login: (userData) => set(state => ({user: userData})),
+  lastMessagesUser: undefined,
+  //performs a login request and fetches user data if successful
+  login: async (username, password) => {
+    let loginResp = await loginRequest(username, password);
+    if (loginResp.user) {
+      let userData = await getUser(loginResp.user);
+      set(state => ({user: {...loginResp, ...userData.user}}))
+    } else {
+      //bad login
+    }
+  },
   logout: () => set(state => ({user: {token: ""}})),
-
+  loadMoreMessages: (username) => set(async state => { //async doesn't change state but can be used to ref previous state before request
+    //if username has changed, use no offset and remove previous messages.
+    const differentUsername = state.lastMessagesUser !== username;
+    //load 10 messages
+    let resp = await getMessages(10, differentUsername ? 0 : state.messages.length, username);
+    set(state => ({
+      messages: [...(differentUsername ? [] : state.messages), ...resp.messages],
+      lastMessagesUser: username
+    }));
+  }),
 });
 
 // create useStore hook

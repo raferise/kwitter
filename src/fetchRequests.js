d9efd6fcf83491554aa1706ec679a6508d8653ea
sunzadok <65696421+sunzadok@users.noreply.github.com>
2021-03-23 13:38:51 -0400

    Add feature/#2-api-get-picture (#16)

--- src/fetchRequests.js
+++ src/fetchRequests.js
@@ -1,3 +1,4 @@
+
 const baseURL = "https://socialapp-api.herokuapp.com/";
 
 export const loginRequest = (username, password) => {
@@ -15,6 +16,12 @@ export const logoutRequest = (token) => {
   return fetch(baseURL + "auth/logout", {
     headers: { Authorization: "Bearer " + token },
   }).then((res) => res.json());
+};
+
+export const pictureRequest = (username) => {
+  return fetch(baseURL + `users/${username}/picture`, {
+  })
+  .then((res) => res.json());
 };
 
 export const deleteMessage = (token, messageId) => {

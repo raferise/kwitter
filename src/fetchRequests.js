1283d1aa8de096b6a4e18817d2fee64e6de607b9
Riley Ferise <10011501+infinitymeme@users.noreply.github.com>
2021-03-23 12:11:06 -0400

    Add feature/#10-delete-user (#33)

--- src/fetchRequests.js
+++ src/fetchRequests.js
@@ -29,3 +29,10 @@ export const createNewUser = (username, displayName, password) => {
     }),
   }).then((res) => res.json());
 }
+
+export const deleteUser = (token, username) => {
+  return fetch(baseURL + "users/" + username, {
+    method: "DELETE",
+    headers: { Authorization: "Bearer " + token },
+  }).then((res) => res.json());
+}

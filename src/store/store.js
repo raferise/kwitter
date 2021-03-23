31af8184e1b321e233b52086e86720685fbfdb85
Riley Ferise <raferise@gmail.com>
2021-03-23 14:23:21 -0400

    store now handles user, message, and alerts management and their appropraiate fetch requests

--- src/store/store.js
+++ src/store/store.js
@@ -2,36 +2,50 @@ import create from "zustand";
 import { devtools } from "zustand/middleware";
 import { getMessages, getUser, loginRequest, logoutRequest } from "../fetchRequests";
 
+const makeAlert = function(entropy, header, body) {
+  return {
+    id: String(Number(new Date()))+entropy,
+    header,
+    body,
+  };
+}
+
 // define reducer function
 const reducer = (set) => ({
+  //USER MANAGEMENT
   user: {token: ""},
-  messages: [],
-  lastMessagesUser: undefined,
-  //performs a login request and fetches user data if successful
   login: async (username, password) => {
     let loginResp = await loginRequest(username, password);
-    if (loginResp.user) {
-      let userData = await getUser(loginResp.user);
+    if (loginResp.username) {
+      let userData = await getUser(loginResp.username);
       set(state => ({user: {...loginResp, ...userData.user}}))
     } else {
-      //bad login
+      set(state => ({alerts:[...state.alerts, makeAlert(state.alerts.length, "Error signing in", loginResp.message)]}));
     }
   },
-  //performs a logout request
-  logout: async () => set(async state => {
+  logout: async () => set(async state => { //refs previous state prefetch
     let resp = await logoutRequest(state.user.token);
-    if (resp.statusCode === 200) set(state => ({user: {token: ""}}));
+    if (resp.statusCode === 200) {
+      set(state => ({user: {token: ""}}));
+    } else {
+      set(state => ({alerts:[...state.alerts, makeAlert(state.alerts.length, "Error signing out", resp.message)]}));
+    }
   }),
-  loadMoreMessages: (username) => set(async state => { //async doesn't change state but can be used to ref previous state before request
+  //MESSAGES LIST MANAGEMENT
+  messages: [],
+  lastMessagesUser: undefined,
+  loadMoreMessages: (username) => set(async state => { //refs previous state prefetch
     //if username has changed, use no offset and remove previous messages.
     const differentUsername = state.lastMessagesUser !== username;
-    //load 10 messages
     let resp = await getMessages(10, differentUsername ? 0 : state.messages.length, username);
     set(state => ({
       messages: [...(differentUsername ? [] : state.messages), ...resp.messages],
       lastMessagesUser: username
     }));
   }),
+  //ALERTS MANAGEMENT
+  alerts: [],
+  removeAlert: (id) => set(state => ({alerts:state.alerts.filter(a => a.id !== id)})),
 });
 
 // create useStore hook

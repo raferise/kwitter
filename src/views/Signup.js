e633d5ee1d1cf2e5a5058a8f1458d9f2f8864854
Timothy <tim.chandler91@gmail.com>
2021-03-24 13:48:29 -0400

    Updated Create Account Page

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,12 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+
 
 function Signup(props) {
   return (
     <>
-     <h1>Signup Placeholder</h1>
+     <Container fluid="md">
+     
+     <h1>Signup for Kwitter!</h1>
+     
+
+     <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Remember me" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</Container>
+     
+
+     
     </>
   );
 }
+
 
 export default Signup;
